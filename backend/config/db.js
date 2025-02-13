03509f7c23232f8b36a5f055ae9b33575dfb7aa8
Zayed Alam <110758359+zayedwithay@users.noreply.github.com>
2025-02-13 23:44:34 +0530

    Update db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,5 +2,5 @@ import mongoose from "mongoose";
 
 export const connectDB = async () => {
 
-    await mongoose.connect('mongodb+srv://zayed:M7ekPLBatOoV5hz@cluster0.zlv1dr5.mongodb.net/EatsExpress').then(()=>console.log("DB Connected"));
+    await mongoose.connect('mongodb+srv://zayed:M7ekPLBatOoV5hzw@cluster0.zlv1dr5.mongodb.net/EatsExpress').then(()=>console.log("DB Connected"));
 }
